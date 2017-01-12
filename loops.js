edafaf2eb34d0a70506b3a746909170b2415542b
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-12 05:53:37 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    array.push("I am ${i} strange loops.")
+  }
+  return array
+}
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do { array.pop()
+  } while (array.length > -1 && maybeTrue());
+  return array
+}
